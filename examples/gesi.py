1aa45454dfc07a3878e7865e8654d54adf71696c
Hyodong Moon <Hyodong.moon@gesi.kr>
2023-03-09 16:45:25 +0900

    3rd commit

--- examples/gesi.py
+++ examples/gesi.py
@@ -17,3 +17,6 @@ name = 'gesi_web_jupyter'
 
 executor = ModelExecutor(data_path, name=name, save_result=True, solver='cplex', verbose=1)
 solver = executor.run_once()
+
+
+asdkfjasldfjasijdfoasdjfoaisdjf
